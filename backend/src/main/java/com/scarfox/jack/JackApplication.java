2ee42ab8c92f69d50e2dfa2832eed70e3665af44
Davi Abner <davi.abner57@gmail.com>
2026-08-03 21:10:05 -0300

    chore: configura ambiente do Spring Boot

---
+++ backend/src/main/java/com/scarfox/jack/JackApplication.java
@@ -0,0 +1,13 @@
+package com.scarfox.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/scarfox/jack/JackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarfox.jack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
