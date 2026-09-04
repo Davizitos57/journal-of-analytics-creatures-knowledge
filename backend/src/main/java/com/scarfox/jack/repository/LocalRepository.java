2ad6d6e86b4a87982858cf25a1ce7e0148ded184
Davi Abner <davi.abner57@gmail.com>
2026-09-04 15:18:07 -0300

    fix(repository): conserta a nomeclatura das funções da entidade 'Local'

--- backend/src/main/java/com/scarfox/jack/repository/LocalRepository.java
+++ backend/src/main/java/com/scarfox/jack/repository/LocalRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.scarfox.jack.domain.entity.Local;
 
-public interface LocalRepository extends JpaRepository<Local, UUID>{
-    List<String> findDistinctPaisOrderByPaisAsc();
-    List<String> findDistinctEstadoProvinciaByPaisIgnoreCaseOrderByEstadoProvinciaAsc(String pais);
-    List<String> findDistinctCidadeByPaisIgnoreCaseAndEstadoProvinciaIgnoreCaseOrderByCidadeAsc(String pais, String estadoProvincia);
-    List<String> findDistinctBairroByPaisIgnoreCaseAndEstadoProvinciaIgnoreCaseAndCidadeIgnoreCaseOrderByBairroAsc(String pais, String estadoProvincia, String cidade);
-    List<String> findDistinctRuaByPaisIgnoreCaseAndEstadoProvinciaIgnoreCaseAndCidadeIgnoreCaseAndBairroIgnoreCaseOrderByRuaAsc(String pais, String estadoProvincia, String cidade, String bairro);
+public interface LocalRepository extends JpaRepository<Local, UUID> {
+    List<Local> findAllByOrderByPaisAsc();
+    List<Local> findByPaisIgnoreCaseOrderByEstadoProvinciaAsc(String pais);
+    List<Local> findByPaisIgnoreCaseAndEstadoProvinciaIgnoreCaseOrderByCidadeAsc(String pais, String estadoProvincia);
+    List<Local> findByPaisIgnoreCaseAndEstadoProvinciaIgnoreCaseAndCidadeIgnoreCaseOrderByBairroAsc(String pais, String estadoProvincia, String cidade);
+    List<Local> findByPaisIgnoreCaseAndEstadoProvinciaIgnoreCaseAndCidadeIgnoreCaseAndBairroIgnoreCaseOrderByRuaAsc(String pais, String estadoProvincia, String cidade, String bairro);
 }
